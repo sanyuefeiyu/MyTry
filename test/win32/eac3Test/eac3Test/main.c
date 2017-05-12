611ba2b861ab6e014c8ce8321fa49e8d1aa5b497
syfy <heshishuang@163.com>
2017-05-13 01:03:46 +0800

    add memory leak check.

--- test/win32/eac3Test/eac3Test/main.c
+++ test/win32/eac3Test/eac3Test/main.c
@@ -1,6 +1,16 @@
+#include <stdlib.h>
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define MY_DEBUG_NEW new(_CLIENT_BLOCK, __FILE__, __LINE__)   
+#define new MY_DEBUG_NEW 
+
 int main(int argc, char **argv)
 {
     TestDecoder();
+
+    _CrtDumpMemoryLeaks(); 
 
     return 0;
 }
