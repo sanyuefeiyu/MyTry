2d7bac4ea74c84b812a9942ce8abe52bee94d060
syfy <heshishuang@163.com>
2017-05-29 17:54:58 +0800

    continue to parse gif format.

--- test/src/testGif.c
+++ test/src/testGif.c
@@ -11,7 +11,7 @@
 #define Magic_Image_Descriptor        0x2C
 #define Magic_Extension_Introducer    0x21
 
-typedef struct DRGB
+typedef struct
 {
     unsigned char alpha;
     unsigned char r;
@@ -19,7 +19,23 @@ typedef struct DRGB
     unsigned char b;
 } DRGB;
 
-typedef struct LocalColorTableFlag
+typedef struct
+{
+    unsigned short logicalScreenWidth;
+    unsigned short logicalScreenHeight;
+
+    unsigned int globalColorTableFlag;
+    unsigned int colorResolution;
+    unsigned int sortFlag;
+    unsigned int pixel;
+
+    unsigned char backgroundColor;
+    unsigned char pixelAspectRatio;
+
+    DRGB globalColorTable[256];
+} LogicalScreenDescriptor;
+
+typedef struct
 {
     unsigned int m;
     unsigned int i;
@@ -28,7 +44,7 @@ typedef struct LocalColorTableFlag
     unsigned int pixel;
 } LocalColorTableFlag;
 
-typedef struct ImageDescriptor
+typedef struct
 {
     unsigned char magic;
     unsigned short offsetX;
@@ -38,7 +54,14 @@ typedef struct ImageDescriptor
     LocalColorTableFlag localColorTable;
 } ImageDescriptor;
 
-typedef struct UserInputFlag
+typedef struct
+{
+    unsigned char magic;
+    unsigned char Label;
+    unsigned char BlockSize;
+} BlockHeader;
+
+typedef struct
 {
     unsigned int reserved;
     unsigned int method;
@@ -46,24 +69,22 @@ typedef struct UserInputFlag
     unsigned int t;
 } UserInputFlag;
 
-typedef struct ApplicationData
+typedef struct
 {
     unsigned char size;
     unsigned char data[256];   // 256 bytes
 } ApplicationData;
 
-typedef struct ExtensionIntroducer
+typedef struct
 {
-    unsigned char magic;
-    unsigned char ExtensionLabel;
-    unsigned char BlockSize;
+    BlockHeader header;
 
-    // 0xF9
+    // 0xF9(Graphic Control Extension)
     UserInputFlag userInputFlag;
     unsigned short DelayTime;
     unsigned char TransparentColorIndex;
 
-    // 0xFF
+    // 0xFF(Application Extension)
     unsigned char ApplicationIdentifier[8]; // 8 bytes
     unsigned char AuthenticationCode[3];  // 3 bytes
     ApplicationData applicationData;  // Data Sub-blocks
@@ -73,24 +94,68 @@ typedef struct ExtensionIntroducer
 
 #define GIF_HEADER_87A  "GIF87a"    // 1987年5月
 #define GIF_HEADER_89A  "GIF89a"    // 1989年7月
-typedef struct DGif
+typedef struct
 {
     unsigned char version[32];
 
-    unsigned short logicalWidth;
-    unsigned short logicalHeight;
-
-    unsigned int globalColorTableFlag;
-    unsigned int colorResolution;
-    unsigned int sortFlag;
-    unsigned int pixel;
-    unsigned char backgroundColor;
-    unsigned char pixelAspectRatio;
-    DRGB globalColorTable[256];
-
+    LogicalScreenDescriptor lsd;
 } DGif;
 
-static int ParserImageDescriptor(gif, bs)
+static int ParseHeader(DGif *gif, void *bs)
+{
+    if (DBitStreamReadBuf(bs, gif->version, strlen(GIF_HEADER_87A)) < 0)
+    {
+        return -1;
+    }
+
+    if (memcmp(gif->version, GIF_HEADER_87A, strlen(GIF_HEADER_87A)) != 0
+        && memcmp(gif->version, GIF_HEADER_89A, strlen(GIF_HEADER_89A)) != 0)
+        return -1;
+
+    return 0;
+}
+
+static int ParseLogicalScreenDescriptor(DGif *gif, void *bs)
+{
+    // get width and height
+    if (DBitStreamReadShort(bs, &gif->lsd.logicalScreenWidth) < 0)
+        return -1;
+    if (DBitStreamReadShort(bs, &gif->lsd.logicalScreenHeight) < 0)
+        return -1;
+
+    unsigned char ch;
+    if (DBitStreamReadChar(bs, &ch) < 0)
+        return -1;
+    gif->lsd.globalColorTableFlag = (ch & 0x80) >> 7;
+    gif->lsd.colorResolution = (ch & 0x70) >> 4;
+    gif->lsd.sortFlag = (ch & 0x08) >> 3;
+    gif->lsd.pixel = (ch & 0x07);
+
+    if (DBitStreamReadChar(bs, &gif->lsd.backgroundColor) < 0)
+        return -1;
+    if (DBitStreamReadChar(bs, &gif->lsd.pixelAspectRatio) < 0)
+        return -1;
+
+    if (gif->lsd.globalColorTableFlag)
+    {
+        int pixelSize = 1 << (gif->lsd.pixel + 1);
+        if (DBitStreamGetLeftSize(bs) < pixelSize * 3)
+            return -1;
+
+        for (int i = 0; i < pixelSize; i++)
+        {
+            unsigned char rgb[3];
+            DBitStreamReadBuf(bs, rgb, 3);
+            gif->lsd.globalColorTable[i].r = rgb[0];
+            gif->lsd.globalColorTable[i].g = rgb[1];
+            gif->lsd.globalColorTable[i].b = rgb[2];
+        }
+    }
+
+    return 0;
+}
+
+static int ParserImageDescriptor(DGif *gif, void *bs)
 {
     if (DBitStreamGetLeftSize(bs) < 10 - 1)
         return -1;
@@ -114,26 +179,26 @@ static int ParserImageDescriptor(gif, bs)
     return 0;
 }
 
-static int ParserExtensionIntroducer(gif, bs)
+static int ParserExtensionIntroducer(DGif *gif, void *bs)
 {
     if (DBitStreamGetLeftSize(bs) < 8 - 1)
         return -1;
 
     ExtensionIntroducer ei;
-    ei.magic = Magic_Extension_Introducer;
+    ei.header.magic = Magic_Extension_Introducer;
+    DBitStreamReadChar(bs, &ei.header.Label);
+    DBitStreamReadChar(bs, &ei.header.BlockSize);
 
-    DBitStreamReadChar(bs, &ei.ExtensionLabel);
-    DBitStreamReadChar(bs, &ei.BlockSize);
-    switch (ei.ExtensionLabel)
+    switch (ei.header.Label)
     {
     case 0xF9:
         {
-            unsigned char temp;
-            DBitStreamReadChar(bs, &temp);
+            unsigned char ch;
+            DBitStreamReadChar(bs, &ch);
             ei.userInputFlag.reserved = 0;
-            ei.userInputFlag.method = (temp & 0x3C) >> 2;
-            ei.userInputFlag.i = (temp & 0x02) >> 1;
-            ei.userInputFlag.t = (temp & 0x01) >> 0;
+            ei.userInputFlag.method = (ch & 0x3C) >> 2;
+            ei.userInputFlag.i = (ch & 0x02) >> 1;
+            ei.userInputFlag.t = (ch & 0x01) >> 0;
             DBitStreamReadShort(bs, &ei.DelayTime);
             DBitStreamReadChar(bs, &ei.TransparentColorIndex);
 
@@ -151,7 +216,7 @@ static int ParserExtensionIntroducer(gif, bs)
         }
         break;
     default:
-        break;
+        return -1;
     }
 
     return 0;
@@ -159,13 +224,16 @@ static int ParserExtensionIntroducer(gif, bs)
 
 static DGif* ParseGif(const char *buf, const int size)
 {
+    // check inputs
     if (buf == NULL || size <= 0)
         return NULL;
 
+    // alloc memory
     DGif *gif = calloc(1, sizeof(DGif));
     if (gif == NULL)
         return NULL;
 
+    // create bitstream
     void *bs = DBitStreamInit((unsigned char*)buf, size);
     if (bs == NULL)
     {
@@ -174,52 +242,12 @@ static DGif* ParseGif(const char *buf, const int size)
     }
 
     // parse header
-    if (DBitStreamReadBuf(bs, gif->version, strlen(GIF_HEADER_87A)) < 0)
-    {
-        free(bs);
-        free(gif);
-        return NULL;
-    }
-    if (memcmp(gif->version, GIF_HEADER_87A, strlen(GIF_HEADER_87A)) != 0
-        && memcmp(gif->version, GIF_HEADER_89A, strlen(GIF_HEADER_89A)) != 0)
-        goto ret;
+    if (ParseHeader(gif, bs) != 0)
+        goto fail;
 
     // parse logical window
-
-    // get width and height
-    if (DBitStreamReadShort(bs, &gif->logicalWidth) < 0)
-        goto ret;
-    if (DBitStreamReadShort(bs, &gif->logicalHeight) < 0)
-        goto ret;
-
-    unsigned char ch;
-    if (DBitStreamReadChar(bs, &ch) < 0)
-        goto ret;
-    gif->globalColorTableFlag = (ch & 0x80) >> 7;
-    gif->colorResolution = (ch & 0x70) >> 4;
-    gif->sortFlag = (ch & 0x08) >> 3;
-    gif->pixel = (ch & 0x07);
-
-    if (DBitStreamReadChar(bs, &gif->backgroundColor) < 0)
-        goto ret;
-    if (DBitStreamReadChar(bs, &gif->pixelAspectRatio) < 0)
-        goto ret;
-
-    if (gif->globalColorTableFlag)
-    {
-        int pixelSize = 1 << (gif->pixel + 1);
-        if (DBitStreamGetLeftSize(bs) < pixelSize * 3)
-            goto ret;
-
-        for (int i = 0; i < pixelSize; i++)
-        {
-            unsigned char rgb[3];
-            DBitStreamReadBuf(bs, rgb, 3);
-            gif->globalColorTable[i].r = rgb[0];
-            gif->globalColorTable[i].g = rgb[1];
-            gif->globalColorTable[i].b = rgb[2];
-        }
-    }
+    if (ParseLogicalScreenDescriptor(gif, bs) != 0)
+        goto fail;
 
     while (DBitStreamGetLeftSize(bs) > 0)
     {
@@ -235,13 +263,17 @@ static DGif* ParseGif(const char *buf, const int size)
             ParserExtensionIntroducer(gif, bs);
             break;
         default:
-            break;
+            goto fail;
         }
     }
 
-ret:
     free(bs);
     return gif;
+
+fail:
+    free(bs);
+    free(gif);
+    return NULL;
 }
 
 const char *filePath0 = "D:\\study\\CS\\res\\gif\\0.gif";
@@ -250,27 +282,27 @@ static void TestParseGif()
 {
     // read data from file
 
-    unsigned char *sourceData = NULL;
-    size_t sourceLen = 0;
+    unsigned char *data = NULL;
+    size_t size = 0;
 
     FILE *fp = fopen(filePath0, "rb+");
     fseek(fp, 0, SEEK_END);
-    sourceLen = ftell(fp);
+    size = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
-    sourceData = malloc(sourceLen);
-    fread(sourceData, sourceLen, 1, fp);
+    data = malloc(size);
+    fread(data, size, 1, fp);
     fclose(fp);
 
     // start to parse Gif
 
-    DGif *gif = ParseGif(sourceData, sourceLen);
+    DGif *gif = ParseGif(data, size);
     if (gif != NULL)
     {
         free(gif);
     }
 
-    free(sourceData);
+    free(data);
 }
 
 void TestGif()
